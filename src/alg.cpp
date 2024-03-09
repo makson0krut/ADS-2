fde17a2f29d56c84c1ba1e8445586d64b4b7405a
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-09 22:53:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,7 +16,7 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
   uint64_t result = 1;
-  for (uint64_t = 1; i <= n; i++) {
+  for (uint64_t i = 1; i <= n; i++) {
     result *= i;
   }
   return result;
