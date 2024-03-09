e6a4d76fcf51d316456c4c26d9d126fc792c09c7
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-09 22:22:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,50 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n == 0) {
+    return 1;
+  }
+  else {
+    uint16_t result = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double result = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    result += pow(x, i) / fact(n);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double result = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    result += pow(-1, i) * (pow(x, 2 * n + 1) / fact(2 * n + 1));
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double result = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    result += pow(-1, i) * pow(x, 2 * i) / fact(2 * i);
+  }
+  return result;
 }
